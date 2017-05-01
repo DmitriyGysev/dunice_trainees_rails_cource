3b20d62378703690c0a018d51271f94d1b968635
Dmitriy <dmitriy.gusev@duniceedge.com>
2017-05-01 20:30:41 +0300

    Changed title

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -5,4 +5,13 @@ module DeviseHelper
     end
     return ''
   end
+
+  def devise_error_messages_for(resource)
+    byebug
+    if resource.errors.full_messages.any?
+      flash.now[:error] = resource.errors.full_messages.join(', ')
+    end
+    return ''
+  end
+
 end
